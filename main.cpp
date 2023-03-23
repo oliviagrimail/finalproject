49727d25136e945c359f41e728a086ac701878e2
oliviagrimail <125220576+oliviagrimail@users.noreply.github.com>
2023-03-22 22:50:01 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,53 +1,142 @@
 #include <iostream>
+#include <vector>
 //Olivia Grimail
 //COP3003
 //Final Project, text based game
-
 //Function for hangman game
-void hangmanGame(){
 
-}
-//Function for Rock, Paper, Scissors Game
-void rpsGame(){
 
-}
-//Function for Tic Tac Toe Game
-void ticTacToeGame(){
-
-}
-
-int main() {
-
+//public and private variables
+class public:
     int game;
+    const int wrongChoice;
+
+class private:
+    int choices;
+
+//function for ascii art
+void displayNumberOfGuesses(){
+    if (wrongChoice == 0){
+        std::cout << "   +_____+\n";
+        std::cout << "   |     |\n";
+        std::cout << "         |\n";
+        std::cout << "         |\n";
+        std::cout << "         |\n";
+        std::cout << " ________|"
+    }
+
+    else if (wrongChoice == 1){
+        std::cout << "   +_____+\n";
+        std::cout << "   |     |\n";
+        std::cout << "   O     |\n";
+        std::cout << "         |\n";
+        std::cout << "         |\n";
+        std::cout << "         |\n";
+        std::cout << " ________|\n";
+
+    }
+
+    else if (wrongChoice == 2) {
+        std::cout << "  +_____+\n";
+        std::cout << "  |     |\n";
+        std::cout << "  O     |\n";
+        std::cout << "  |     |\n";
+        std::cout << "        |\n";
+        std::cout << "________|\n";
+
+    }
+
+    else if (wrongChoice == 3){
+        std::cout << "  +_____+\n";
+        std::cout << "  |     |\n";
+        std::cout << "  O     |\n";
+        std::cout << "  |     |\n";
+        std::cout << "   \\    |\n";
+        std::cout << "________|\n";
+
+    }
+
+    else if (wrongChoice == 4){
+        std::cout << "  +_____+\n";
+        std::cout << "  |     |\n";
+        std::cout << "  O     |\n";
+        std::cout << "  |\\    |\n";
+        std::cout << "   \\    |\n";
+        std::cout << "________|\n";
+
+
+    }
+
+    else if (wrongChoice == 5){
+        std::cout << "  +_____+\n";
+        std::cout << "  |     |\n";
+        std::cout << "  O     |\n";
+        std::cout << " /|\\    |\n";
+        std::cout << "   \\    |\n";
+        std::cout << "________|\n";
+
+    }
+    else if (wrongChoice == 6){
+        std::cout << "  +_____+\n";
+        std::cout << "  |     |\n";
+        std::cout << "  O     |\n";
+        std::cout << " /|\\    |\n";
+        std::cout << " / \\    |\n";
+        std::cout << "________|\n";
+    }
+
+}
+//first game mode that will be added
+void gameMode1(){
+    int wrongChoices;
+    bool validationcheck == false;
+    char letterChoice;
+    std::string input = "";
+    std::string answer = "telephone";
+    std::cout << "Hint: It has many rings, but no fingers.\n What is your first guess?(You only get 6.)\n";
+    std::cin >> input;
+
+    while (input != answer && wrongChoices < 7){
+        displayNumberOfGuesses(wrongChoices);
+
+        std::cout << "Please enter your answer here: ";
+        std::cin >> letterChoice;
+        std::cout << "\n";
+
+        for (int i=0; i<answer.length();i++){
+            if(letterChoice==answer[i]){
+                bool validationCheck = true;
+            }
+        }
+        if (validationcheck = true){
+            std::cout << "Yes! Good job :)\n";
+        }
+        else{
+            std::cout << "Wrong.\n";
+        }
+    }
+
+}
+
+//greeting function for hangman
+void hangmanGame(){
+    std::cout << "Welcome to hangman! The goal of the game is to guess what object I am thinking of! \n The more wrong guesses, the closer the man gets to getting hanged!\n"
+                 "Don't worry though, I'll give you a hint on what it is. \n Ready to begin?";
+
+}
+//mainfunction
+int main() {
+    
     std::string input;
 
-    std::cout << "Would you like to play a game? Enter Y for yes.\n";
+    std::cout << "Would you like to play hangman? Enter Y for yes.\n";
     std::cin >> input;
 
     //loop to let them continue playing games
     while (input == "Y") {
 
-        //Asking user for what game they want to play
-        std::cout<< "Enter a number for which game you would like to play. \n \t1. Rock, Paper, Scissors \n \t2. Hang-man \n \t3. Tic-Tac-Toe \n";
-        std::cin >> game;
-
-        //Checking to see if their input was valid
-        if (game < 1 || game > 3) {
-            std::cout << "Sorry, please try again. Input was invalid.\n";
-        }
-
-        //if user wants to play RPS
-        if(game = 1){
-            rpsGame();
-        }
-        //if user wants to play hangman
-        if (game = 2){
-            hangmanGame();
-        }
-        //if user wants to play Tic-Tac-Toe
-        if (game = 3){
-            ticTacToeGame();
-        }
+        //function to call hangman game
+        hangmanGame();
 
         //to let user stop playing if they want to
         std::cout << "Do you want to play another game?\n";
